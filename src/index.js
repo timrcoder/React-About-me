c8b76145c204547fbf199e1b5e29aaf8b6d833cc
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-6.local>
2018-08-02 18:32:08 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+ReactDOM.render(
+    <div>
+        <h1>About me</h1>
+        <img src="https://i.ytimg.com/vi/pID_QuyUi98/maxresdefault.jpg" height ="200" width="300"/>
+        <p>lorem ipsum Get free texting and calling on WiFi with the free TextNow app. Get affordable no contract smart phones and plans from TextNow Wireless.</p> 
+        <h3>Contact Me</h3>
+        <input placeholder="Name"/>
+        <div>
+            <textarea></textarea>
+        </div>
+        <button>Submit</button>
+    </div>,
+    document.getElementById('root')
+);
+
+
